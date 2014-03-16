12e7e5eb956d65df6e4998d4cbcf7fa023cc145d
Uri Mikhli <urimikhli@gmail.com>
2014-03-15 23:57:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pricecomparison::Application.config.secret_key_base = '19682c973fd7f5eb28eb40f819351a745e02f0e8163b7097cbf98844ded673340f77df1a8ce0389548dc80ee4bb3da8830c2f7a6f08b27f38f5c2cd8dd3f7d2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pricecomparison::Application.config.session_store :cookie_store, key: '_pricecomparison_session'
